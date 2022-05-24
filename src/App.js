50417745246ebccca98e9b34b0aac85cf388caa5
Michael Martin <mj@dashboardmike.com>
2022-05-23 22:27:28 -0500

    added counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

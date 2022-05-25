5571e634b1460c90e68467eb7e189a516b596df0
Michael Martin <mj@dashboardmike.com>
2022-05-25 08:04:18 -0500

    Restructured files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { Counter } from './features/counter/Counter';
+import { CounterPage } from './pages/CounterPage/CounterPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CounterPage />
       </header>
     </div>
   );
---
+++ src/pages/CounterPage/CounterPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Counter } from './counter/Counter'
+
+export function CounterPage() {
+  return (
+    <div>
+      <span>
+      CounterPage
+      </span>
+      <span>
+        <Counter />
+      </span>
+    </div>
+  )
+}

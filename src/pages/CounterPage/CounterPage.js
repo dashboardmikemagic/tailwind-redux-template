00764d79c5d4ceab869c79aa844ea953977f1563
Michael Martin <mj@dashboardmike.com>
2022-05-26 10:25:56 -0500

    Removed duplicate text in counter page

--- src/pages/CounterPage/CounterPage.js
+++ src/pages/CounterPage/CounterPage.js
@@ -4,9 +4,6 @@ import { Counter } from './counter/components/Counter'
 export function CounterPage() {
   return (
     <div>
-      <h1>
-      CounterPage
-      </h1>
       <span>
         <Counter />
       </span>

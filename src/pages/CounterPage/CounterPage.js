8e77083b1fd1e3b12e37fd575836fc5431d4b1fe
Michael Martin <mj@dashboardmike.com>
2022-05-26 10:16:07 -0500

    Setup Tailwind

--- src/pages/CounterPage/CounterPage.js
+++ src/pages/CounterPage/CounterPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Counter } from './counter/Counter'
+import { Counter } from './counter/components/Counter'
 
 export function CounterPage() {
   return (
     <div>
-      <span>
+      <h1>
       CounterPage
-      </span>
+      </h1>
       <span>
         <Counter />
       </span>
--- src/pages/CounterPage/counter/components/Counter.js
+++ src/pages/CounterPage/counter/components/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from '../../../redux/counter/counterSlice'
+import { decrement, increment } from '../../../../redux/counter/counterSlice'
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value)
@@ -11,6 +11,7 @@ export function Counter() {
       <div>
         <button
           aria-label="Increment value"
+          class="bg-sky-600 hover:bg-sky-700 focus:ring-blue-300"
           onClick={() => dispatch(increment())}
         >
           Increment
@@ -18,6 +19,7 @@ export function Counter() {
         <span>{count}</span>
         <button
           aria-label="Decrement value"
+          class="bg-blue-100 hover:bg-blue-400 active:bg-blue-300 focus:outline-none focus:ring focus:ring-blue-200"
           onClick={() => dispatch(decrement())}
         >
           Decrement
